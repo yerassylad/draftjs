69fcc4a57a68de9acb692c396cda8552a0c4df5e
yerassyl <yerassyl.ad@gmail.com>
2018-10-15 17:03:56 +0600

    bold italic underline funcs od texteditor

--- src/text_editor.js
+++ src/text_editor.js
@@ -1,39 +1,43 @@
 import React, { Component } from "react";
 import { Editor, EditorState, RichUtils } from "draft-js";
+import "./text-editor.css";
 
 class TextEditor extends Component {
   constructor(props) {
     super(props);
     this.state = { editorState: EditorState.createEmpty() };
     this.onChange = editorState => this.setState({ editorState });
-    this.handleKeyCommand = this.handleKeyCommand.bind(this);
-  }
-
-  handleKeyCommand(command, editorState) {
-    const newState = RichUtils.handleKeyCommand(editorState, command);
-    if (newState) {
-      this.onChange(newState);
-      return "handled";
-    }
-    return "not-handled";
   }
 
   _onBoldClick() {
     this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, "BOLD"));
   }
 
-  //   render() {
-  //     return <div>TEXT EDITOR</div>;
-  //   }
+  _onItalicClick() {
+    this.onChange(
+      RichUtils.toggleInlineStyle(this.state.editorState, "ITALIC")
+    );
+  }
+
+  _onUnderlineClick() {
+    this.onChange(
+      RichUtils.toggleInlineStyle(this.state.editorState, "UNDERLINE")
+    );
+  }
+
   render() {
     return (
       <div>
         <button onClick={this._onBoldClick.bind(this)}>BOLD</button>
-        <Editor
-          editorState={this.state.editorState}
-          handleKeyCommand={this.handleKeyCommand}
-          onChange={this.onChange}
-        />
+        <button onClick={this._onItalicClick.bind(this)}>ITALIC</button>
+        <button onClick={this._onUnderlineClick.bind(this)}>UNDERLINE</button>
+        <div className="editor-container">
+          <Editor
+            editorState={this.state.editorState}
+            handleKeyCommand={this.handleKeyCommand}
+            onChange={this.onChange}
+          />
+        </div>
       </div>
     );
   }

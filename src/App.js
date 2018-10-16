89e72f3a5dfa3b99d9eb3b093548596fa8c98444
yerassyl <yerassyl.ad@gmail.com>
2018-10-16 19:18:18 +0600

    refactoring

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>HELLO YERASSYL</h1>
         <TextEditor />
       </div>
     );
--- src/text_editor.js
+++ src/text_editor.js
@@ -2,14 +2,64 @@ import React, { Component } from "react";
 import { Editor, EditorState, RichUtils } from "draft-js";
 import "./text-editor.css";
 
-// в darftjs имплементированы BOLD ITALIC UNDERLINE CODE стили. Создаем кастомный стиль для зачеркнутых слов
-const styleMap = {
-  STRIKETHROUGH: {
+// в darftjs имплементированы BOLD ITALIC UNDERLINE стили. Создаем кастомный стиль для зачеркнутых слов
+const StyleMap = {
+  STRIKE: {
     textDecoration: "line-through"
-  },
-  FONT_SIZE: {
-    fontSize: "30px"
   }
+};
+
+// Виды линейных стилей
+const INLINE_STYLES = [
+  { label: "Bold", style: "BOLD" },
+  { label: "Italic", style: "ITALIC" },
+  { label: "Underline", style: "UNDERLINE" },
+  { label: "Strikethrough", style: "STRIKE" }
+];
+
+// Виды блочных стилей
+const BLOCK_STYLES = [
+  { label: "UL", style: "unordered-list-item" },
+  { label: "OL", style: "ordered-list-item" }
+];
+
+// Кнопка с функцией
+const ButtonWithFn = props => {
+  const onPress = () => {
+    props.onToggle(props.style);
+  };
+  return <button onMouseDown={onPress}>{props.label}</button>;
+};
+
+// Рендеринг кнопок для линейных декораций
+const LineDecoratonBtns = props => {
+  return (
+    <div>
+      {INLINE_STYLES.map(iStyle => (
+        <ButtonWithFn
+          key={iStyle.label}
+          label={iStyle.label}
+          style={iStyle.style}
+          onToggle={props.onToggle}
+        />
+      ))}
+    </div>
+  );
+};
+
+const BlockDecorationBtns = props => {
+  return (
+    <div>
+      {BLOCK_STYLES.map(bStyle => (
+        <ButtonWithFn
+          key={bStyle.label}
+          label={bStyle.label}
+          style={bStyle.style}
+          onToggle={props.onToggle}
+        />
+      ))}
+    </div>
+  );
 };
 
 class TextEditor extends Component {
@@ -17,86 +67,34 @@ class TextEditor extends Component {
   constructor(props) {
     super(props);
     this.state = { editorState: EditorState.createEmpty() };
+    this.focus = () => this.refs.focus();
     this.onChange = editorState => this.setState({ editorState });
-    this.handleKeyCommand = this.handleKeyCommand.bind(this);
-    this.textAlignment = "right";
+    this.toggleInlineStyle = this._toggleInlineStyle.bind(this);
+    this.toggleBlockType = this._toggleBlockType.bind(this);
   }
 
-  // обработка комбинации клавиш для стилей
-  handleKeyCommand(command, editorState) {
-    const newState = RichUtils.handleKeyCommand(editorState, command);
-
-    if (newState) {
-      this.onChange(newState);
-      return "handled";
-    }
-
-    return "not-handled";
-  }
-
-  //   переключения стилей оформленения текста
-  _onBoldClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, "BOLD"));
-  }
-
-  _onItalicClick() {
+  _toggleInlineStyle(inlineStyle) {
     this.onChange(
-      RichUtils.toggleInlineStyle(this.state.editorState, "ITALIC")
+      RichUtils.toggleInlineStyle(this.state.editorState, inlineStyle)
     );
   }
 
-  _onUnderlineClick() {
-    this.onChange(
-      RichUtils.toggleInlineStyle(this.state.editorState, "UNDERLINE")
-    );
-  }
-
-  _onStrikethroughClick() {
-    this.onChange(
-      RichUtils.toggleInlineStyle(this.state.editorState, "STRIKETHROUGH")
-    );
-  }
-
-  _onBulletpointsClick() {
-    this.onChange(
-      RichUtils.toggleBlockType(this.state.editorState, "unordered-list-item")
-    );
-  }
-
-  _onOrderedpointsClick() {
-    this.onChange(
-      RichUtils.toggleBlockType(this.state.editorState, "ordered-list-item")
-    );
-  }
-
-  _hugeFontSizeClick() {
-    this.onChange(
-      RichUtils.toggleInlineStyle(this.state.editorState, "FONT_SIZE")
-    );
+  _toggleBlockType(blockType) {
+    this.onChange(RichUtils.toggleBlockType(this.state.editorState, blockType));
   }
 
   render() {
     return (
       <div>
-        <button onClick={this._onBoldClick.bind(this)}>BOLD</button>
-        <button onClick={this._onItalicClick.bind(this)}>ITALIC</button>
-        <button onClick={this._onUnderlineClick.bind(this)}>UNDERLINE</button>
-        <button onClick={this._onStrikethroughClick.bind(this)}>
-          STRIKETHROUGH
-        </button>
-        <button onClick={this._onBulletpointsClick.bind(this)}>
-          BULLET POINTS
-        </button>
-        <button onClick={this._onOrderedpointsClick.bind(this)}>
-          ORDERED POINTS
-        </button>
-        <button onClick={this._hugeFontSizeClick.bind(this)}>Huge</button>
+        <LineDecoratonBtns onToggle={this.toggleInlineStyle} />
+        <BlockDecorationBtns onToggle={this.toggleBlockType} />
         <div className="editor-container">
           <Editor
-            customStyleMap={styleMap}
+            customStyleMap={StyleMap}
             editorState={this.state.editorState}
-            handleKeyCommand={this.handleKeyCommand}
             onChange={this.onChange}
+            placeholder="Введите текст"
+            ref="Текстовый редактор"
           />
         </div>
       </div>

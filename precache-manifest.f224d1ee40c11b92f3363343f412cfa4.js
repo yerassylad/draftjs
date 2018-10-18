1c07acc0c8808a47c92d8cfeb3da2b1284befe41
yerassyl <yerassyl.ad@gmail.com>
2018-10-18 16:10:33 +0600

    Updates

---
+++ precache-manifest.f224d1ee40c11b92f3363343f412cfa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "afb39be0c80d60a419d1",
+    "url": "/draftjs/static/js/runtime~main.afb39be0.js"
+  },
+  {
+    "revision": "176efb99b79ae28d5416",
+    "url": "/draftjs/static/js/main.176efb99.chunk.js"
+  },
+  {
+    "revision": "3b7465833848b7d410af",
+    "url": "/draftjs/static/js/1.3b746583.chunk.js"
+  },
+  {
+    "revision": "176efb99b79ae28d5416",
+    "url": "/draftjs/static/css/main.8fd13870.chunk.css"
+  },
+  {
+    "revision": "9831225c2cba1eee181bf4314b4b3fa9",
+    "url": "/draftjs/index.html"
+  }
+];
